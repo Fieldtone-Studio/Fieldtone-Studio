fd7ad3b4f4b7cbd918313bba5d002933e2d660ad
Fieldtone-Studio <Fieldtonestudio@gmail.com>
2026-02-23 11:20:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const loader = document.getElementById("loader");
     const mainContent = document.getElementById("main-content");
 
+    // Lock scroll
+    document.documentElement.classList.add("loading");
     document.body.classList.add("loading");
 
     if (sessionStorage.getItem("fieldtoneLoaderShown")) {
+
         loader.classList.add("hidden");
         mainContent.classList.add("visible");
+
+        document.documentElement.classList.remove("loading");
         document.body.classList.remove("loading");
+
     } else {
+
         setTimeout(() => {
+
             loader.classList.add("hidden");
             mainContent.classList.add("visible");
+
+            document.documentElement.classList.remove("loading");
             document.body.classList.remove("loading");
+
             sessionStorage.setItem("fieldtoneLoaderShown", "true");
-        }, 4000);
+
+        }, 3600); // perfectly synced with animation timing
     }
 
 });
